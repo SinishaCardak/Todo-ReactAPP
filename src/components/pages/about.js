fd8a73c06c3d4aa7c6f372530b804958d7d09828
SinishaCardak <sinisa_cardak@hotmail.com>
2019-10-02 13:52:21 +0200

    Initial commit

---
+++ src/components/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function about() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the ToDoList app v1.0.0. It is part of a 
+            React Crash course</p>
+        </React.Fragment>
+            
+        
+    );
+}
+
+export default about;
